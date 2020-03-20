2b23e0b9a1e485fc7cedf0d9f5fb4cdf56b00eb2
J Antonio Gomez <a01374459@itesm.mx>
2020-03-20 15:38:51 -0600

    3 commit

--- interprete.py
+++ interprete.py
@@ -1,8 +1,33 @@
 
+variables={}
+stack=[]
 
+def checkOpcode(linea):
+    if linea[0]=="mov":
+       linea[1]=linea[1].split(',')
+       variables[linea[1][0]]=linea[1][1]
+
+    elif linea[0]=="add":
+        linea[1]=linea[1].split(',')
+        operando1=int(variables[linea[1][0]])
+        operando2=int(variables[linea[1][1]])
+        suma=operando1+operando2
+        variables[linea[1][0]]=suma
+    
+    elif linea[0]=="push":
+        stack.append(variables[linea[1]])
+
+
+def runCode(archivo):
+    for linea in archivo:
+        linea=linea.replace('\r','').replace('\n','')
+        linea=linea.split(" ")
+        checkOpcode(linea)
 
 def main():
-
-    print("Yei")
+    archivo=open('ensamblador.txt')
+    runCode(archivo)
+    print(variables)
+    print(stack)
 
 main()
